97ded1514556d8292592c5182860cf21aaf1f55d
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2026-03-04 20:10:36 +0000

    feat: add form input component

---
+++ src/app/components/form-input/form-input.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { AbstractControl, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-form-input',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './form-input.component.html',
+  styleUrl: './form-input.component.css'
+})
+export class FormInputComponent {
+  @Input() label: string = '';
+  @Input() placeholder: string = '';
+  @Input() type: string = 'text';
+  @Input() control!: AbstractControl;
+  @Input() errorMessage: string = '';
+
+  get formControl(): FormControl {
+    return this.control as FormControl;
+  }
+}
