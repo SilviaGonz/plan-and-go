da7667fa371f5a4ce601a7fbf39e2ee511adda6e
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2026-03-04 20:10:21 +0000

    feat: update button component styles and structure

--- src/app/components/button-with-bg/button-with-bg.component.ts
+++ src/app/components/button-with-bg/button-with-bg.component.ts
@@ -1,16 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-button-with-bg',
   standalone: true,
-  imports: [RouterLink, NgStyle],
+  imports: [NgStyle],
   templateUrl: './button-with-bg.component.html',
-  styleUrl: './button-with-bg.component.css'
+  styleUrl: './button-with-bg.component.css',
 })
 export class ButtonWithBgComponent {
   @Input() text: string = '';
   @Input() route: string = '';
   @Input() color: string = 'var(--color-primary)';
+  @Input() type: string = 'button';
+  @Input() disabled: boolean = false;
+  @Input() loadingText: string = '';
+  @Input() variant: 'filled' | 'outline' = 'filled';
 }
