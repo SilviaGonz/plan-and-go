7e1b758859b33ec38d806c1f2e49f9224d24b935
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2026-03-04 20:09:54 +0000

    feat: configure Firebase integration and add environment settings

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBd5Id_tIyFRbDf_IcPwc7VEZ3MI-RV42Y",
+    authDomain: "plan-and-go-af4ff.firebaseapp.com",
+    projectId: "plan-and-go-af4ff",
+    storageBucket: "plan-and-go-af4ff.firebasestorage.app",
+    messagingSenderId: "476549008779",
+    appId: "1:476549008779:web:312707be49871aea9ab38e"
+  }
+};
