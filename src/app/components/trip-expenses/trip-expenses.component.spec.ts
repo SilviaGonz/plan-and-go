b966afe995ab4fd462927aa0fdee843e33dc5732
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2026-04-14 16:41:48 +0100

    feat: add trip expenses component with

---
+++ src/app/components/trip-expenses/trip-expenses.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TripExpensesComponent } from './trip-expenses.component';
+
+describe('TripExpensesComponent', () => {
+  let component: TripExpensesComponent;
+  let fixture: ComponentFixture<TripExpensesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TripExpensesComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(TripExpensesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
